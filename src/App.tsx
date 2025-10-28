ff0ce36f2e7617705ac4a6e9e6bbe16861a3ab2b
xzero11x <7358385022@untrm.edu.pe>
2025-10-27 23:04:19 -0500

    feat(init): configurar React, Vite y Tailwind CSS v4

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold text-blue-500 underline">
+        ¡Frontend Ferretería Configurado!
+      </h1>
+    </div>
+  )
+}
+
+export default App
